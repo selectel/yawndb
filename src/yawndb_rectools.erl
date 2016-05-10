c5f6f0d965807dfcdb8dcbea8704a21291e3ff33
Pavel Abalihin <abalihin@selectel.ru>
2016-05-10 15:34:53 +0300

    Cleanup old code

--- src/yawndb_rectools.erl
+++ src/yawndb_rectools.erl
@@ -1,15 +1,6 @@
 -module(yawndb_rectools).
 -include_lib("yawndb.hrl").
 -compile({parse_transform, exprecs}).
--export([f1/0, f2/0, f3/0]).
 
 -export_records([rule, conveyor, p_conveyor]).
 
-f1() ->
-    {new,'#new-rule'([])}.
-
-f2() ->
-    {new,'#new-conveyor'([])}.
-
-f3() ->
-    {new, '#new-p_conveyor'([])}.
